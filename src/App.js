f875b2efcb64f603825a2d0a83acdf4711ff16f4
Aden Au <wlau.2014@sis.smu.edu.sg>
2018-02-20 15:34:19 +0800

    clean dir, add endpoint to App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      data: null
+    }
+  }
+
+  async componentDidMount () {
+    const { data } = await axios.get('http://pb-api.herokuapp.com/bars')
+    this.setState(data)
+  }
+
+  render () {
+    const { data } = this.state
+    console.log(data)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Welcome to my progress bars</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
